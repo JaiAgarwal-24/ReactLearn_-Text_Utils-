18710802effe9d2ef42e10175fdfebd169ead992
Jai Agarwal <jai.agarwal2022@gmail.com>
2022-09-14 12:18:26 +0530

    adding logics using js like preview and count

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,20 +8,36 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handleLoClick = () => {
+        // console.log("Uppercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
     const handleOnChange = (event) =>{
         // console.log("On Change");
         setText(event.target.value);
     }
 
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
     // setText("new text");
     return (
+        <>
         <div className='container my-3'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
         </div>
+        <div className="container my-2">
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Mintues to read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
